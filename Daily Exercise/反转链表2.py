7f8970af990fc816ce9594a1aecd47c8a77073cf
aberfield <liufangyou521@163.com>
2021-03-20 00:33:25 +0800

    算法日练

---
+++ Daily Exercise/反转链表2.py
@@ -0,0 +1,34 @@
+import ListNode
+class Solution:
+    def reverseBetween(self, head: ListNode, left: int, right: int) -> ListNode:
+        def reverse_linked_list(head: ListNode):
+            pre = None
+            cur = head
+            while cur:
+                next = cur.next
+                cur.next = pre
+                pre = cur
+                cur = next
+
+
+        dummy_node = ListNode(-1)
+        dummy_node.next = head
+        pre = dummy_node
+
+        for _ in range(left - 1):
+            pre  = pre.next
+
+        right_node = pre
+        for _ in range(right - left + 1):
+            right_node = right_node.next
+
+        left_node = pre.next
+        curr = right_node.next
+
+        pre.next = None
+        right_node.next = None
+
+        reverse_linked_list(left_node)
+        pre.next = right_node
+        left_node.next = curr
+        return dummy_node.next
