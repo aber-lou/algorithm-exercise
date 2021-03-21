ada58bff5b67d87c96bd6798124ac7f63fa365b9
aberfield <liufangyou521@163.com>
2021-03-21 23:23:22 +0800

    矩阵置0

---
+++ Daily Exercise/矩阵置0(常数空间).java
@@ -0,0 +1,53 @@
+class Solution{
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+
+        boolean r0 = false, c0 = false;
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                r0 = true;
+                break;
+            }
+        }
+
+        for (int j = 0; j < n; j++) {
+            if(matrix[0][j] == 0) {
+                c0 = true;           
+            }
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for (int j = 1; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                for (int i = 1; i < m; i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            if(matrix[i][0] == 0) {
+                Arrays.fill(matrix[i],0);
+            }
+        }
+
+        if(r0) {
+            for (int i = 0; i < m; i ++) {
+                matrix[i][0] = 0;
+            }
+        }
+
+        if(c0) {
+            Arrays.fill(matrix[0],0);
+        }
+
+    }
+}
+    
