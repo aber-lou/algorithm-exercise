3ec7f661d311ec039251967b3e098b784f9c3171
aberfield <liufangyou521@163.com>
2021-04-14 23:52:12 +0800

    Create Trie前缀树.cpp

---
+++ Daily Exercise/Trie前缀树.cpp
@@ -0,0 +1,19 @@
+class Trie {
+    public:
+
+    Trie() {
+
+    }
+
+    void insert(string word) {
+
+    }
+
+    bool search(string word) {
+
+    }
+
+    bool startWith(string prefix) {
+        
+    }
+}
