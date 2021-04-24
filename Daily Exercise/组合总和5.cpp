23fc0a82df8654264f49ecb140d7d65a309b5ff0
aberfield <liufangyou521@163.com>
2021-04-25 00:15:17 +0800

    Update 组合总和5.cpp

--- Daily Exercise/组合总和5.cpp
+++ Daily Exercise/组合总和5.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        vector<int> dp(target+1,0);
+        vector<int> dp(target + 1);
         dp[0] = 1;
         for (int i = 1; i  <= target; i++) {
-            for(auto a :nums) {
-                dp[i] += dp[i-a];
+            for (int& num : nums) {
+                if (num <= i && dp[i - num] < INT_MAX - dp[i]) {
+                    dp[i] += dp[i - num];
+                }
             }
         }
-        return dp.back();
+        return dp[target];
     }
 };
