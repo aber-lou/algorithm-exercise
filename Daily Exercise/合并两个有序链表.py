13c74776c3f7bcda89120544181734d7d90a4a15
aberfield <liufangyou521@163.com>
2021-06-21 23:23:58 +0800

    Update 合并两个有序链表.py

--- Daily Exercise/合并两个有序链表.py
+++ Daily Exercise/合并两个有序链表.py
@@ -30,8 +30,11 @@ class Solution:
             return l1
         elif l1.val < l2.val:
             l1.next = self.mergeTwoListPlanB(self,l1.next,l2)
+            return l1
         else:
             l2.next = self.mergeTwoListPlanB(self,l1,l2.next)
+            return l2
+        
             
     def __init__(self) -> None:
         self.mergeTwoListPlanB()
