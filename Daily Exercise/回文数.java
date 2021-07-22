11818bb996e5e2d3a5347c492a4513ebfd62fdba
aberfield <liufangyou521@163.com>
2021-07-22 23:49:54 +0800

    Update 回文数.java

--- Daily Exercise/回文数.java
+++ Daily Exercise/回文数.java
@@ -1,12 +1,14 @@
 class Solution {
-    if ( x < 0) {
-        return false;
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int cur = 0;
+        int num = x;
+        while (n != 0) {
+            cur = cur * 10 + num % 10;
+            num /= 10;
+        }
+        return cur == x;
     }
-    int cur = 0;
-    int num = x;
-    while (n != 0) {
-        cur = cur * 10 + num %10;
-        num /= 10;
-    }
-    return cur == x;
 }
