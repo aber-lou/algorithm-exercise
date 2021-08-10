a2bf4ff37918b83e27848193f7c927f3ce4cea2e
aberfield <liufangyou521@163.com>
2021-08-11 00:29:54 +0800

    Merge branch 'master' of https://github.com/aberfield/algorithm-exercise
    change

--- Daily Exercise/LRU.java
+++ Daily Exercise/LRU.java
@@ -4,5 +4,14 @@ public class LRU {
 
 
 class Node {
+    public int key;
+    public int val;
+    public Node prev;
+    public Node next;
     
+    public Node(int key, int val) {
+        this.key = key;
+        this.val = val;
+        
+    }
 }
