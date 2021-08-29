9a472bc4273d0261b086e0192a4fc9878ce693e0
aberfield <liufangyou521@163.com>
2021-08-30 00:05:57 +0800

    Update LFUCache.java

--- Daily Exercise/LFUCache.java
+++ Daily Exercise/LFUCache.java
@@ -1,14 +1,47 @@
-public class LFUCache {
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
 
-}
+import org.graalvm.compiler.graph.Node;
 
-class LinkedList {
-    Node head;
-    Node tail;
+class LFUCache {
+    int minfreq, capacity;
+    Map<Integer, Node> key_table;
+    Map<Integer, LinkedList<Node>> freq_table;
 
-    public LinkedList() {
-        this.head = new Node(0, 0, 100000);
+    public LFUCache(int capacity) {
+        this.capacity = capacity;
+        this.minfreq = 0;
+        this.key_table = new HashMap<Integer, Node>();
+        this.freq_table = new HashMap<Integer, LinkedList<Node>>()
     }
+
+    public int get(int key) {
+        if(capacity == 0) {
+            return -1;
+        }
+
+        if(!key_table.containsKey(key)) {
+            return -1;
+        }
+
+
+        Node node = key_table.get(key);
+        int val = node.val, freq = node.frequency;
+        freq_table.get(freq).remove(node);
+        if(freq_table.get(freq).size() == 0) {
+            freq_table.remove(freq);
+            if(minfreq == freq) {
+                minfreq += 1;
+            }
+        }
+        LinkedList<Node> list = freq_table.getOrDefault(freq+1, new LinkedList<Node>());
+        list.offerFirst(new Node(key,val,freq+1));
+        freq_table.put(freq+1, list);
+        key_table.put(key, freq_table.get(freq+1).peekFirst());
+        return val;
+    }
+
 }
 
 class Node {
