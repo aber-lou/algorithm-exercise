d5a48e5c905472f29b6dada20599819024b1269a
aberfield <liufangyou521@163.com>
2021-09-02 23:54:00 +0800

    Create 将N叉树编码为二叉树.cpp

---
+++ Daily Exercise/将N叉树编码为二叉树.cpp
@@ -0,0 +1,27 @@
+class Node {
+    public:
+        int val;
+        vector<Node*> children;
+
+        Node(){}
+
+        Node(int _val) {
+            val = _val;
+        }
+
+        Node(int _val, vector<Node*> _children) {
+            val = _val;
+            children = _children;
+        }
+};
+
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x): val(x), left(nullptr), right(nullptr){}
+};
+
+
